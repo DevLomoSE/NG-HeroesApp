465eba09d4bd05b6c01905b79152dc6398ec07d5
DevLomoSE <jonathancggs@gmail.com>
2021-10-03 17:40:38 -0500

    :heavy_plus_sign: Http Module
    adding the imports for http client and refactor the order of imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
 @NgModule({
@@ -15,6 +15,8 @@ import { ErrorPageComponent } from './shared/error-page/error-page.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
+    
     AppRoutingModule
   ],
   providers: [],
