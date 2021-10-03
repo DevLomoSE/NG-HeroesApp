05eb26687ff8477bea75423a9192af3197302ef1
DevLomoSE <jonathancggs@gmail.com>
2021-10-03 14:22:26 -0500

    :jigsaw: Agregar
    creating the add component for heroes

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AgregarComponent } from './app/agregar/agregar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AgregarComponent
+  ],
   imports: [
     CommonModule
   ]
