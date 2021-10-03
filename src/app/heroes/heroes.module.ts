ed68c5c779885b62b55bc3428633d0281373ce54
DevLomoSE <jonathancggs@gmail.com>
2021-10-03 14:23:29 -0500

    :recycle: Agregar
    refactor code to use the correct path to the module

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AgregarComponent } from './app/agregar/agregar.component';
+import { AgregarComponent } from './pages/agregar/agregar.component';
 
 
 
