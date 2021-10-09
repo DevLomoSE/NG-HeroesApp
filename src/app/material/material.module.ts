59273e00bd507f9fa99d96433a7d1776ad8195ed
DevLomoSE <jonathancggs@gmail.com>
2021-10-09 18:32:08 -0500

    :heavy_plus_sign: Material

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
-import {MatSidenavModule} from '@angular/material/sidenav'; 
-
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card'; 
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    MatSidenavModule
+  exports: [
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatGridListModule,
+    MatListModule,
+    MatProgressSpinnerModule,
+    MatSidenavModule,
+    MatToolbarModule
   ]
 })
 export class MaterialModule { }
