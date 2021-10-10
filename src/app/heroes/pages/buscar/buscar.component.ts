3679205ac6f6a0f310bbc488858da82450ef7a05
DevLomoSE <jonathancggs@gmail.com>
2021-10-10 18:42:13 -0500

    :bento: Autocomplete
    adding the functionality of autocomplete on search

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { Heroe, Publisher } from '../../interfaces/heroes';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-buscar',
@@ -8,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuscarComponent implements OnInit {
 
-  constructor() { }
+  termino: string;
+  heroes: Heroe[];
+  selectedHeroe?: Heroe | undefined;
+
+  constructor(
+    private heroesService: HeroesService
+  ) {
+    this.termino = '';
+    this.heroes = [];
+  }
 
   ngOnInit(): void {
+  }
+
+  buscando(){
+    this.heroesService.getSugerencias(this.termino).subscribe(response => this.heroes = response);
+  }
+
+  selectedOption( event: MatAutocompleteSelectedEvent ){
+    this.selectedHeroe = undefined;
+    if(event.option.value !== ""){
+      const heroe: Heroe = event.option.value;
+      this.termino = heroe.superhero;
+      
+      this.heroesService.getHeroeById( heroe.id! )
+                        .subscribe( heroe => this.selectedHeroe = heroe);
+    }
   }
 
 }
