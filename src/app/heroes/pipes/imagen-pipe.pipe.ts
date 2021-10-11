d0fd328e3d497955095aa842516ab06b733569f1
DevLomoSE <jonathancggs@gmail.com>
2021-10-10 20:10:39 -0500

    :recycle: Imagen pipe
    refactoring the pipe to return the url based on multiple evaluations

--- src/app/heroes/pipes/imagen-pipe.pipe.ts
+++ src/app/heroes/pipes/imagen-pipe.pipe.ts
@@ -9,7 +9,15 @@ export class ImagenPipe implements PipeTransform {
   img_url!: string;
 
   transform(heroe?: Heroe, ...args: unknown[]): unknown {
-    this.img_url  = (heroe?.id) ? `assets/heroes/${heroe?.id}.jpg`: `assets/no-image.png`;
+
+    if( !heroe?.id && !heroe?.alt_img ){
+      this.img_url = `assets/no-image.png`;
+    }else if ( heroe?.alt_img ){
+      this.img_url = heroe?.alt_img;
+    }else{
+      this.img_url = `assets/heroes/${heroe?.id}.jpg`
+    }
+
     return this.img_url;
   }
 
