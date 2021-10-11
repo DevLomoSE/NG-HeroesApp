8654bb7a00eeb3eda6833951e57b32b7fb00bd3a
DevLomoSE <jonathancggs@gmail.com>
2021-10-10 19:47:48 -0500

    :necktie: Edit
    adding logic to edit a heroe

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 import { Heroe, Publisher } from '../../interfaces/heroes';
 import { HeroesService } from '../../services/heroes.service';
 
@@ -28,19 +30,39 @@ export class AgregarComponent implements OnInit {
   };
 
   constructor(
-    private heroesService: HeroesService
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params
+                        .pipe( 
+                          switchMap( ({id}) => this.heroesService.getHeroeById(id) )
+                        )
+                        .subscribe( heroe => this.heroe = heroe );
+  }
+
+  setAction(){
+
   }
 
   guardarHeroe(){
-    console.log(this.heroe)
-
     if(this.heroe.superhero.trim().length === 0){
       return;
     }
-    this.heroesService.saveHeroe(this.heroe).subscribe( response => console.log(response));
+
+    if( this.heroe.id){
+      this.heroesService.updateHeroe(this.heroe)
+                        .subscribe( heroe => {
+                                                console.log(heroe)
+                                             });
+    }else{
+      this.heroesService.saveHeroe(this.heroe)
+                        .subscribe( heroe => {
+                                                this.router.navigate(['/heroes/editar', heroe.id]);
+                                             });
+    }
   }
 
 }
