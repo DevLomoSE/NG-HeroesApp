474a89ffa02c16ba4b1f253db549a78806041a66
DevLomoSE <jonathancggs@gmail.com>
2021-10-10 19:29:18 -0500

    :bento: Save
    adding the html form to save heroe

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,16 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { Heroe, Publisher } from '../../interfaces/heroes';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
-  styles: [
-  ]
+  styles: [`
+    img{
+      width: 100%;
+    }
+  `]
 })
 export class AgregarComponent implements OnInit {
 
-  constructor() { }
+  publishers =[
+    { id: 'DC Comics', desc: 'DC - Comics'},
+    { id: 'Marvel Comics', desc: 'Marvel - Comics'},
+  ];
+
+  heroe: Heroe = {
+    superhero: '',
+    alter_ego: '',
+    characters: '',
+    first_appearance: '',
+    publisher: Publisher.DCComics,
+    alt_img: '',
+  };
+
+  constructor(
+    private heroesService: HeroesService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  guardarHeroe(){
+    console.log(this.heroe)
+
+    if(this.heroe.superhero.trim().length === 0){
+      return;
+    }
+    this.heroesService.saveHeroe(this.heroe).subscribe( response => console.log(response));
   }
 
 }
